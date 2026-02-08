cef7329d185c52869afc4f50dd96117fa323d5e6
powermate <218421152+powermate25@users.noreply.github.com>
2026-02-08 01:03:19 +0000

    Routine save: Battleship project progress 🔔 - Defining Game board class methods.

--- src/app/driver.js
+++ src/app/driver.js
@@ -1,107 +1,114 @@
+const clog = console.log
+clog("Battleship")
 
-function capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-}
-
-function reverseString(str) {
-    return str.split("").toReversed().join("")
-}
-
-class calculator {
-    constructor(a, b) {
-        this.a = a
-        this.b = b
-    }
-    add(a, b) {
-        return a + b
-    }
-
-    subtract(a, b) {
-        return a - b
-    }
-
-    divide(a, b) {
-        return a / b
-    }
-
-    multiply(a, b) {
-        return a * b
-    }
-}
-
-function caesarCipher(str, shiftNum) {
-    const base = Array.from(str)
-    .map(i => {
-        // Checking if char is case sensitive (letter) or not (symbol)
-        // Used lower and upper case combo as trick.
-        // Opposed to letters, symbols to lower or upper case won't change. 
-        if ( i.toLowerCase().toUpperCase() 
-        === i.toLowerCase()
-        ) {i}
-        else {
-            const init = i
-            function revertChar(char) {
-                if(init.toUpperCase() === init ) {return char.toUpperCase()}
-                else { return char.toLowerCase()}
-
-            // detecting if char code will be above 122 ("z") 
-            // if so simply loop to "a" and add remaining shifting number
-            // subtracting -1 at the end because "a" is the +1 added (loop) char
-            }
-            function detectCharLoop(i) {
-                const tempBase = i.toLowerCase().charCodeAt()
-                if (tempBase + shiftNum > 122) {
-                    const tempShift = (tempBase + shiftNum) - 122
-                    const loopedChar = ("a".charCodeAt() + tempShift) - 1
-                    return i = String.fromCharCode(loopedChar)
-                }
-                else {
-                    const temp = i.charCodeAt() + shiftNum
-                    i = String.fromCharCode(temp)
-                    // console.log(i)
-                    return i
-                }
-            }
-            i = detectCharLoop(i)
-            // revert shifted char to match initial character case 
-            i = revertChar(i)
-        }
-        return i
-    })
-
-    return base.join("")
-}
-
-class analyzeArray {
-    constructor (arr) {
-        // this.arr = arr
-        this.average = this.avg(arr)
-        this.min = this.min(arr)
-        this.max = this.max(arr)
-        this.length = arr.length
-        
-    }
-
-    avg(arr) {
-        const result = arr.reduce( (acc, curr) => {
-            return (acc + curr)
-        }, 0 )
-        return Math.floor( (result / arr.length) )
-    }
-
-    min(arr) {
-        const result = arr.reduce( (acc, curr) => {
-            return ( acc = acc < curr ? acc : curr )
-        })
-        return result
-    }
-
-    max(arr) {
-        const result = arr.reduce( (acc, curr) => {
-            return ( acc = acc > curr ? acc : curr )
-        })
-        return result
+// Code
+class Ship {
+    constructor(shipLength) {
+        this.ship = new Array(shipLength)
+        this.length = this.ship.length
+        this.hits = 0
+        this.sunken = this.isSunk()
     }
     
+    hit() {
+        this.hits += 1
+        this.sunken = this.hits >= this.length
+        ? true : false
+    }
+
+    isSunk() {
+        return this.hits >= this.length
+        ? true : false
+    }
 }
-export { capitalize, reverseString, calculator, caesarCipher, analyzeArray  }
+
+function createGraphBfs(x = 5) {
+    x = x - 1
+    const cells = []
+    for (let i = 0; i <= x; i++) {
+        function buildCells(num) {
+            for(let id = 0; id <= num; id++) {
+                cells.push([i, id])
+            }
+        }
+        buildCells(x)
+    }
+    return cells
+}
+
+class GameBoard {
+    constructor(gridNum) {
+        this.board = createGraphBfs(gridNum)
+        this.occupiedPositions = []
+        this.boardLimit = gridNum - 1
+    }
+
+    placeShip(ship, dir, posX ) {
+        const limit = this.boardLimit
+        let posY
+    
+        function checkPosition(posY) {
+            posY[0] = posY[0] > limit 
+            ? false : posY[0] < 0 ? false
+            : posY[0]
+
+            posY[1] = posY[1] > limit
+            ? false : posY[1] < 0 ? false
+            : posY[1]
+            
+        }
+
+        const length = ship.length
+        clog(length)
+        if(length > limit || length < 0) {
+            throw new Error(`Maximum ship size is ${limit}`)
+        }
+        // dir = dir === "v" ? "v" : "h"
+        posY = dir === "v-"
+        ? [posX[0] - (length-1), posX]
+        : dir === "v+"
+        ? [posX[0] + (length-1), posX[1] ]
+        : dir === "h-"
+        ? [posX[0], posX[1] - (length-1) ]
+        : dir === "h+"
+        ? [posX[0], posX[1] + (length-1) ]
+        : false
+        
+        //
+        if (dir === "v-") {
+            posY = [posX[0] - (length-1), posX[1] ]
+            clog( checkPosition(posY) )
+            clog(posY)
+        }
+        else if (dir === "v+") {
+            posY = [posX[0] + (length-1), posX[1] ]
+            clog( checkPosition(posY) )
+            clog(posY)
+        }
+        else if (dir === "h-") {
+            posY = [posX[0], posX[1] - (length-1) ]
+            clog( checkPosition(posY) )
+            clog(posY)
+        }
+        else if (dir === "h+") {
+            posY = [posX[0], posX[1] + (length-1) ]
+            clog( checkPosition(posY) )
+            clog(posY)
+        }
+        
+        
+        this.occupiedPositions.push([])
+    }
+}
+
+
+
+
+
+
+// Logs
+const demoShip = new Ship(4)
+const gameBoard = new GameBoard(10)
+clog(demoShip)
+clog(gameBoard)
+clog( gameBoard.placeShip(demoShip, "h+", [6, 1] ) )
